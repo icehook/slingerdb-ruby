eb8b474a81233b26d87ff49675c0b63198eded0a
Keith Larrimore <keithlarrimore@gmail.com>
2012-12-07 13:10:21 -0500

    latest

--- test/download.rb
+++ test/download.rb
@@ -9,13 +9,10 @@ endpoint, api_key, file = ARGV[0], ARGV[1], ARGV[2]
 
 SlingerDB.config =  { :endpoint => endpoint, :api_key => api_key }
 
-response = Request.get '/downloads.json'
-
 f = File.open(ARGV[2], 'w')
 f.puts "<html>\n<body>\n"
 
-response.env[:body][:downloads].each do |attributes|
-  d = Download.new attributes
+Download.all.each do |d|
   f.puts "<b>id:</b> #{d.id} <b>status:</b> #{d.status} <b>cdr_count:</b> #{d.cdr_count} <b>download:</b> <a href='#{d.download_uri}?share_key=#{d.share_key}'>#{d.name}</a><br/>"
 end
 
